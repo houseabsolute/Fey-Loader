7ffbdcfb92cc8ff783ebe94793055c94ba7aacf9
autarch <devnull@localhost>
2008-02-02 22:02:23 +0000

    Add some POD.

--- lib/Fey/Loader.pm
+++ lib/Fey/Loader.pm
@@ -49,7 +49,54 @@ EOF
     return $class . '::' . 'DBI';
 }
 
-
 1;
 
 __END__
+
+=head1 NAME
+
+Fey::Loader - Load your schema defintion from a DBMS
+
+=head1 SYNOPSIS
+
+  my $loader = Fey::Loader->new( dbh => $dbh );
+
+  my $schema = $loader->make_schema();
+
+=head1 DESCRIPTION
+
+C<Fey::Loader> takes a C<DBI> handle and uses it to construct a set of
+Fey objects representing that schema. It will attempt to use an
+appropriate DBMS subclass if one exists, but will fall back to using a
+generic loader otherwise.
+
+The generic loader simply uses the various schema information methods
+specified by C<DBI>. This in turn depends on these methods being
+implemented by the driver.
+
+=head1 METHODS
+
+This class provides the following methods:
+
+=head2 Fey::Loader->new( dbh => $dbh )
+
+Given a connected C<DBI> handle, this method returns a new loader. If
+an appropriate subclass exists, it will be loaded and used. Otherwise,
+it will warn and fall back to using L<Fey::Loader::DBI>.
+
+=head1 AUTHOR
+
+Dave Rolsky, <autarch@urth.org>
+
+=head1 BUGS
+
+See C<Fey::Core> for details on how to report bugs.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006-2008 Dave Rolsky, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
